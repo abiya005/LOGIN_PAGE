05bd3b7cb9f99d35224869b399cb41234c90d2e2
abiya005 <abiyabiju050805@gmail.com>
2025-01-19 16:48:38 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginRegister from './Components/LoginRegister/LoginRegister';
 
 function App() {
   return (
     <div>
-      
+      <LoginRegister/>
     </div>
   );
 }
