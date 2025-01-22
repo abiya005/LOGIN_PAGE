8e1b89613e12662cca6ce373a1bc2f53454ff226
abiya005 <abiyabiju050805@gmail.com>
2025-01-22 22:19:25 +0530

    jsx changed

--- src/Components/LoginRegister/LoginRegister.jsx
+++ src/Components/LoginRegister/LoginRegister.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState }from 'react';
 import './LoginRegister.css';
-import { FaUser, FaLock } from "react-icons/fa";
+import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
 
 
 const LoginRegister = () => {
+
+  const [action, setAction ] = useState('');
+  const registerLink = ()  => {
+    setAction('active');
+  };
+  const loginLink = ()  => {
+    setAction('active');
+  };
   return (
-    <div className='wrapper'>
+    <div className={'wrapper${action}'}>
       <div className="form-box login">
         <form action="">
           <h1>Login</h1>
@@ -18,11 +26,36 @@ const LoginRegister = () => {
             <FaLock className="icon"/>
           </div>
           <div className="remember-forget">
-            <label><input type="checkbox"/>Remind me</label><a href="#">  Forgot Password ?</a>
+            <label><input type="checkbox"/>Remember me </label><a href="#">Forgot password ?</a>
           </div>
           <button type="submit">Login</button>
           <div className="register-link">
-            <p>Don't have an account ?<a href="#">  Register</a></p>
+            <p>Don't have an account ?<a href="#" onClick={registerLink}>Register</a></p>
+          </div>
+
+        </form>
+      </div>
+      <div className="form-box register">
+        <form action="">
+          <h1>Registration</h1>
+          <div className="input-box">
+            <input type="text" placeholder='Username' required />
+            <FaUser className="icon"/>
+          </div>
+          <div className="input-box">
+            <input type="email" placeholder='Email' required />
+            <FaEnvelope  className="icon"/>
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder='Password' required />
+            <FaLock className="icon"/>
+          </div>
+          <div className="remember-forget">
+            <label><input type="checkbox"/>I agee to the TERMS & CONDITIONS</label>
+          </div>
+          <button type="submit">Register</button>
+          <div className="register-link">
+            <p>Already have an account ?<a href="#" onClick={loginLink}>Login</a></p>
           </div>
 
         </form>
